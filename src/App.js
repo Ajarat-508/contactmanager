2f21d32cf9154aa5d28c227b24fb6f3a53d42e8f
Ajarat-508 <129962602+Ajarat-508@users.noreply.github.com>
2023-10-29 23:34:20 +0100

    commit a change

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import React from 'react';
+import Contact from './Component/Contact';
+import Header from './Component/header';
+
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>The App Component</h1>
+    <div className="App">  
+      <Header branding="Contact Manager"/>
+      <div className='container'> 
+        <Contact name="Ajarat Azeez" 
+        email="ajarat@example.com" 
+        phone="+234812345678" />
+
+        <Contact name="Uloka Ngozi" 
+        email="gift@example.com" 
+        phone="+234812345678" />
+
+      </div>
     </div>
   );
 }
